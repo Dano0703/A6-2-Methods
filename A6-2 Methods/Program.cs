46cd1738cca74cbd4b75bad51e5ed94492f09aba
Dano0703 <Daniel.jayy.smith@gmail.com>
2022-02-10 14:10:22 -0700

    Add project files.

---
+++ A6-2 Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace A6_2_Methods
+{
+    class Temperature_Conversion
+    {
+        static void Main()
+        {
+            int f;
+
+            double result;
+
+            Console.WriteLine("Enter degrees in Fahrenheit: ");
+             f = int.Parse(Console.ReadLine());
+            result = (double)(5.0 / 9.0 * (f - 32));
+            Console.WriteLine($"It is {result:0.##} degrees Celcius");
+            Console.ReadLine();
+
+            int c;
+
+            double Result;
+
+            Console.WriteLine("Enter degrees in celcius: ");
+            c = int.Parse(Console.ReadLine());
+            Result = (double)(9.0 / 5.0 * c + 32);
+            Console.WriteLine($"It is {Result:0.##} degrees Fahrenheit");
+            Console.ReadLine();
+
+
+
+        } 
+    }
+}
